82201114d465efaf90e566f3648db20dfe8d4797
Katt Baum <mkb2@orlhs164wk-osx.housing.admin.nyu.edu>
2020-01-29 14:10:18 -0500

    Clean table display

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 let userCount = 0;
 let pageCount = 0;
 
-console.log('users', userCount)
-
 const usersList = document.querySelector('#usersList');
 const pageList = document.querySelector('#pages')
 
@@ -11,40 +9,33 @@ function renderUsers(users){
     const html=users.users.map(user=>{
         return `
             <tr>
-              <td>${user.fullName}</td>
+              <td>${user.firstName}</td>
+              <td>${user.lastName}</td>
               <td>${user.email}</td>
               <td>${user.title}</td>
             </tr>`
     });
-    html.unshift("<tr><th>Full Name</th><th>Email</th><th>Title</th></tr>")
+    html.unshift('<tr><th>First Name</th><th>Last Name</th><th>Email</th><th>Title</th></tr>')
     html.join('')
     usersList.innerHTML = html;
 }
 
-function renderPages(){
-  console.log(pageCount);
-  let html = []
-  for (let i = 0; i < pageCount; i++) {
-      html.push(`<div>${i}</div>`)
-  }
-  html.join('')
-  console.log(html)
-  pageList.innerHTML = html;
-}
-
-renderPages()
+// function renderPages(){
+//   console.log(pageCount);
+//   let html = []
+//   for (let i = 0; i < pageCount; i++) {
+//       html.push(`<div>${i}</div>`)
+//   }
+//   html.join('')
+//   console.log(html)
+//   pageList.innerHTML = html;
+// }
 
 fetch('https://acme-users-api-rev.herokuapp.com/api/users') 
-.then (response =>response.json())
-.then( users =>{
+.then (response => response.json())
+.then( users => {
     userCount = users.count;
     pageCount = Math.round(userCount/50, 0)
     console.log('page', pageCount)
     renderUsers(users);
 });
-
-function displayPages() {
-  console.log(pageCount)
-}
-
-displayPages()
