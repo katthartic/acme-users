02ffb446223c1187ad4b6b7b63bef9032ca08a49
katthartic <katt.baum@gmail.com>
2020-01-29 23:53:09 -0500

    Add bookmarkable hash

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
 const usersList = document.querySelector('#usersList')
 const pageList = document.querySelector('#pages')
+const pages = document.querySelector('#pages')
+let idx = window.location.hash.slice(1)
+
+pages.addEventListener('click', (ev) => {
+  const target = ev.target
+  const targetTag = target.tagName
+  const parentTag = target.parentElement.tagName
+  console.log(target, targetTag, parentTag)
+
+  if (targetTag === 'A' && parentTag === 'DIV') {
+  target.parentElement.classList.add('selected')
+  }
+})
+
+window.addEventListener('hashchange', () => {
+  idx = window.location.hash.slice(1)
+  goFetch(idx)
+})
+
+if (idx) {
+  goFetch(idx)
+} else {
+  goFetch(0)
+}
 
 function renderUsers(users){
-  console.log(users)
-  const html = users.map(user=>{
+  const html = users.map(user => {
     return `<tr>
       <td>${user.firstName}</td>
       <td>${user.lastName}</td>
       <td>${user.email}</td>
       <td>${user.title}</td>
-      </tr>`
-          
+      </tr>`   
   })
   html.unshift('<tr><th>First Name</th><th>Last Name</th><th>Email</th><th>Title</th></tr>')
   usersList.innerHTML = html.join('')
 }
-
 
 function renderPages(count){
   let pageTotal = Math.ceil(count / 50, 0)
@@ -28,15 +49,11 @@ function renderPages(count){
   pageList.innerHTML = html.join('')
 }
 
-fetch('https://acme-users-api-rev.herokuapp.com/api/users') 
-.then(response => response.json())
-.then(response => {
-  renderUsers(response.users)
-  renderPages(response.count)
+function goFetch (id) {
+  fetch(`https://acme-users-api-rev.herokuapp.com/api/users/${id}`)
+    .then(response => response.json())
+    .then(response => {
+      renderUsers(response.users)
+      renderPages(response.count)
 })
-
-//only returns 50
-//connect page to idx of user
-//fill array based on idx + 49
-
-
+}
